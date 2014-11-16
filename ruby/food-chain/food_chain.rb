74e0acf1c0e69ae96b4d33f2a0518cc6bf40bdab
Evan Moore <etmoore@gmail.com>
2014-11-15 21:55:14 -0700

    completed Bob

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,5 +1,10 @@
 class FoodChainSong
 
+  verses = [
+              ["I know an old lady who swallowed a fly.",
+              "I don't know why she swallowed the fly. Perhaps she'll die."]
+            ]
+
   def verse(verse_count)
     verse_count += 1
     song = []
