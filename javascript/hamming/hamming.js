fbe85d41929ad33ac056543312f0d04b0980aef0
Evan Moore <etmoore@gmail.com>
2015-02-08 18:14:32 -0700

    moved compute within module exports

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,14 +1,14 @@
-var compute = function(a, b) {
-  if (a.length !== b.length)
-    throw new Error('DNA strands must be of equal length.');
-  distance = 0;
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] !== b[i])
-      distance += 1;
-  }
-  return distance;
-};
-
 module.exports = {
-  compute: compute
+
+  compute: function(a, b) {
+    if (a.length !== b.length)
+      throw new Error('DNA strands must be of equal length.');
+    distance = 0;
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] !== b[i])
+        distance += 1;
+    }
+    return distance;
+  }
+
 };
