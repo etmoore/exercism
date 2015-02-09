00b20926dfa6d6043eaf1aef1ba36c6769f9a695
Evan Moore <etmoore@gmail.com>
2015-02-08 17:59:09 -0700

    added missing semicolon

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,6 +1,6 @@
 var compute = function(a, b) {
   if (a.length !== b.length)
-    throw new Error('DNA strands must be of equal length.')
+    throw new Error('DNA strands must be of equal length.');
   distance = 0;
   for (var i = 0; i < a.length; i++) {
     if (a[i] !== b[i])
