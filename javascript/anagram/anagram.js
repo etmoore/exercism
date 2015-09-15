578b1ddcb3d188db26578c1ac7e1728f57e9ad98
Evan Moore <etmoore@gmail.com>
2015-09-14 19:55:03 -0600

    passing all tests

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,26 +2,30 @@ function anagram(word){
   return {
     word: word,
     matches: function(targetWords){
+      // If arguments is not an array, convert arguments list into an array.
+      if (!Array.isArray(arguments[0])){
+        targetWords = Array.prototype.slice.call(arguments);
+      }
+
       var matches = [];
       var sourceWord = this.word;
       for (var i = 0; i < targetWords.length; i++){
         var targetWord = targetWords[i];
-        if (this.avgChars(sourceWord) === this.avgChars(targetWord)){
+        if (avgChars(sourceWord) === avgChars(targetWord)){
           matches.push(targetWord);
         }
       }
       return matches;
-      // if (this.word === 'ant') return ['tan'];
-      // else return [];
-    },
-    avgChars: function(string){
-      // use reduce to clean this up
-      var sum = 0;
-      for (var i = 0; i < string.length; i++){
-        sum += string.charCodeAt(i)
-      }
-      return sum / string.length;
     }
   }
 };
+
+var avgChars = function(string){
+  var sum = 0;
+  for (var i = 0; i < string.length; i++){
+    sum += string.charCodeAt(i)
+  }
+  return sum / string.length;
+}
+
 module.exports = anagram;
