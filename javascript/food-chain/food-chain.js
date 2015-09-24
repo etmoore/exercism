e1a88e9a0608a6ee2f5ced761d1ac697a2d2b6a6
Evan Moore <etmoore@gmail.com>
2015-09-23 17:22:28 -0700

    passed first test in food chain

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,7 @@
+var song = {
+  verse: function(){
+    return "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n";
+  }
+}
+
+module.exports = song;
