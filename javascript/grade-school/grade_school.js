097ea20a65c7f8e998aa2f60aa8e02af1bfdae1e
Evan Moore <etmoore@gmail.com>
2015-09-25 15:41:39 -0400

    can add students to different grades

--- javascript/grade-school/grade_school.js
+++ javascript/grade-school/grade_school.js
@@ -6,10 +6,10 @@ School.prototype.roster = function(){
   return this.gradeLevels;
 };
 School.prototype.add = function(student, grade){
-  if(!this.gradeLevels[2]){
-    this.gradeLevels[2] = [];
+  if(!this.gradeLevels[grade]){
+    this.gradeLevels[grade] = [];
   }
-  this.gradeLevels[2].push(student);
+  this.gradeLevels[grade].push(student);
 };
 
 module.exports = School;
