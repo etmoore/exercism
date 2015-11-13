4b139fa220b37a62d536b9868fcf3eefe9738683
Evan Moore <etmoore@gmail.com>
2015-11-13 06:24:19 -0700

    formatting

--- javascript/grade-school/grade_school.js
+++ javascript/grade-school/grade_school.js
@@ -8,10 +8,12 @@ School.prototype.roster = function(){
   }
   return this.gradeLevels;
 };
+
 School.prototype.add = function(student, grade){
   this.gradeLevels[grade] = this.gradeLevels[grade] || [];
   this.gradeLevels[grade].push(student);
 };
+
 School.prototype.grade = function(grade){
   var students = this.gradeLevels[grade];
   return students ? students.sort() : [];
