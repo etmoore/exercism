b1cc40f5a2a642732694f828e23adcc72f026637
Evan Moore <etmoore@gmail.com>
2016-08-31 22:19:07 -0600

    pass all tests for leap javascript

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,7 +2,7 @@ function Year(year){
   this.year = year;
 }
 Year.prototype.isLeap = function(){
-  if (this.year === 2016)
+  if (this.year % 4 === 0 && (this.year % 100 !== 0 || this.year % 400 === 0))
     return true;
   return false;
 };
