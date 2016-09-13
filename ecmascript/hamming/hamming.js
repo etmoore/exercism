59708447da3746334d24e05f60fbee03dc6f369e
etmoore <etmoore@gmail.com>
2016-09-12 22:58:23 -0600

    calculate hamming distance for all strands; hamming exercise complete

--- ecmascript/hamming/hamming.js
+++ ecmascript/hamming/hamming.js
@@ -1,7 +1,11 @@
 class Hamming {
   compute(a, b) {
-    if (a === b) return 0;
-    else return 1;
+    if (a.length !== b.length) throw Error('DNA strands must be of equal length.')
+    let count = 0;
+    for (var i = 0; i < a.length; i++){
+      if (a[i] !== b[i]) count += 1;
+    }
+    return count;
   }
 }
 module.exports = Hamming;
