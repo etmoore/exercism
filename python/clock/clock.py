2c99840852a88c8a8ea2e7b5a2c9626133b1b228
etmoore <etmoore@gmail.com>
2016-09-15 23:59:29 -0600

    initialize hours and minutes attributes

---
+++ python/clock/clock.py
@@ -0,0 +1,8 @@
+class Clock():
+    '''Clock that displays 24 hour time'''
+
+    def __init__(self, hour, minute):
+        self.hour = hour
+        self.minute = minute
+
+    # def __repr__(self):
