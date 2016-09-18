c9b4bf69e483433ab028f44c301e2eda6378d6b2
Evan Moore <etmoore@gmail.com>
2016-09-17 23:54:41 -0600

    pass clock exercise

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,8 +1,23 @@
 class Clock():
-    '''Clock that displays 24 hour time'''
+    'converts hours and minutes to 24 hour format'
 
-    def __init__(self, hour, minute):
-        self.hour = hour
-        self.minute = minute
+    def __init__(self, hours, minutes):
+        self.hours = hours
+        self.minutes = minutes
+        self.convert_to_24_hour()
 
-    # def __repr__(self):
+    def __repr__(self):
+        return "%02d:%02d" % (self.hours, self.minutes)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def add(self, minutes):
+        self.minutes += minutes
+        return self.convert_to_24_hour()
+
+    def convert_to_24_hour(self):
+        self.hours += self.minutes // 60
+        self.hours %= 24
+        self.minutes %= 60
+        return self
